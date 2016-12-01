714649f3d092cc70fb154c2b7ffe84a73b19d9aa
vitormdias <vitormd97@gmail.com>
2016-12-01 19:14:37 -0200

    Find Project solution added

---
+++ LearnYouMongo/findProject.js
@@ -0,0 +1,25 @@
+const mongo = require('mongodb').MongoClient
+
+const url = 'mongodb://localhost:27017/learnyoumongo';
+
+const inputAge = parseInt(process.argv[2]);
+
+mongo.connect(url , function (err,db) {
+    if(err)
+        throw err;
+    let parrots = db.collection('parrots')
+        .find({
+            age: {
+                $gt: inputAge
+            }
+        }, {
+            age: 1,
+            name: 1,
+            _id: 0
+        }).toArray(function (err , documents) {
+            if(err)
+                throw err;
+            console.log(documents);
+            db.close();
+        });
+});
