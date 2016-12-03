e3f7a32c75605301dd9942dae164eace9cdffa51
vitormdias <vitormd97@gmail.com>
2016-12-03 00:27:43 -0200

    Remove added

---
+++ LearnYouMongo/remove.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/' + process.argv[2];
+
+mongo.connect(url, function(err, db) {
+  if(err)
+    throw err;
+
+  let collection = db.collection(process.argv[3]);
+
+  collection.remove({
+    _id: process.argv[4]
+  }, function (err) {
+    if(err) throw err;
+    db.close();
+  });
+});
