40b1b6b0e6529658bca013fa3af41b78e2c36863
vitormdias <vitormd97@gmail.com>
2016-12-04 17:58:08 -0200

    Added count solution

---
+++ LearnYouMongo/count.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/learnyoumongo';
+
+const param = process.argv[2];
+
+mongo.connect(url, function(err, db) {
+  if(err)
+    throw err;
+
+  let collection = db.collection('parrots');
+
+  collection.count({
+      age: {
+        $gt: +param
+      }
+  }, function (err,count) {
+        console.log(count);
+        db.close();
+  });
+
+});
