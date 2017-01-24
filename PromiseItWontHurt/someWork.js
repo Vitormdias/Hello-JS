508127b76b84a3fa988251ff4b6e3feda9586414
Vitor Manfré <vitormd97@gmail.com>
2017-01-24 17:04:09 -0200

    some work done

---
+++ PromiseItWontHurt/someWork.js
@@ -0,0 +1,11 @@
+const qhttp = require('q-io/http');
+
+qhttp.read("http://localhost:7000/")
+.then((id)  => {
+  return qhttp.read("http://localhost:7001/" + id);
+})
+.then((json) => {
+  console.log(JSON.parse(json));
+})
+.then(null, console.error)
+.done();
